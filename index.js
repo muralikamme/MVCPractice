a2aed92cf1b1e8fc793c9789f5ed60c380a3e192
Prashanth yejje <pyejje@bitbucket.allmysons.com>
2024-10-14 21:23:50 +0530

    changes in files

--- index.js
+++ index.js
@@ -2,6 +2,27 @@ var express=require("express")
 
 var app=express()
 
+var mysql2=require("mysql2")
+
+
+
+var conn=mysql2.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"Murali@339",
+    database:"fristdata"
+})
+
+
+conn.connect((err)=>{
+    if(err){
+        console.log(err);
+    }else{
+   console.log("database connected");
+    }
+
+})
+
 app.get("/",(req,res)=>{
 
     res.send("hello")
